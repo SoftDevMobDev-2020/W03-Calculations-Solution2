7e7dd3b857e76dd34a226d2e4d06648997a75da3
N. A. Ronald <nronald@swin.edu.au>
2020-08-21 15:54:31 +1000

    Edited video version to use higher order functions and a lambda for the radio group listener.

--- app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
@@ -2,51 +2,32 @@ package au.edu.swin.sdmd.w03_calculations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioButton
-import android.widget.TextView
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
-    var operator = "plus"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var operator: ((String, String) -> Int) = ::add
 
         val number1 = findViewById<EditText>(R.id.number1)
         val number2 = findViewById<EditText>(R.id.number2)
         val answer = findViewById<TextView>(R.id.answer)
 
         val equals = findViewById<Button>(R.id.equals)
-        equals.setOnClickListener { _ ->
-            val result = when(operator) {
-                "plus" -> add(number1.text.toString(), number2.text.toString())
-                "mult" -> mult(number1.text.toString(), number2.text.toString())
-                else -> add(number1.text.toString(), number2.text.toString())
-            }
-                // TODO: show result on the screen
+        equals.setOnClickListener {
+            val result = operator(number1.text.toString(), number2.text.toString())
             answer.text = result.toString()
         }
 
-    }
+        val radioGroup = findViewById<RadioGroup>(R.id.radioOp)
 
-    fun onRadioButtonClicked(view: View) {
-        if (view is RadioButton) {
-            // Is the button now checked?
-            val checked = view.isChecked
-
-            // Check which radio button was clicked
-            when (view.getId()) {
-                R.id.radioPlus ->
-                    if (checked) {
-                        operator = "plus"
-                    }
-                R.id.radioMult ->
-                    if (checked) {
-                        operator = "mult"
-                    }
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            val rb = group.findViewById<RadioButton>(checkedId)
+            when (rb.id) {
+                R.id.radioPlus -> operator = ::add
+                R.id.radioMult -> operator = ::mult
             }
         }
     }
